91765dde9db6a70affd9124f342431c70df6ab71
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2024-10-02 14:16:42 +0100

    Fix default indentation style.

--- src/main/java/fish/payara/fishmaps/FishMapsMain.java
+++ src/main/java/fish/payara/fishmaps/FishMapsMain.java
@@ -14,51 +14,51 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FishMapsMain implements ModInitializer {
-	public static final String MODID = "fishmaps";
+    public static final String MODID = "fishmaps";
     public static final Logger LOGGER = LoggerFactory.getLogger("FishMaps");
-	private static final int THREADS = 1; // WARNING: Increasing this value can overload the Payara Server!
+    private static final int THREADS = 1; // WARNING: Increasing this value can overload the Payara Server!
 
-	public static Thread[] callThreads = new Thread[THREADS];
-	private static final HttpCaller[] callers = new HttpCaller[THREADS];
+    public static Thread[] callThreads = new Thread[THREADS];
+    private static final HttpCaller[] callers = new HttpCaller[THREADS];
 
-	@Override
-	public void onInitialize () {
-		Settings.read();
-		ServerChunkEvents.CHUNK_LOAD.register(Messenger::cacheChunk);
-		ServerTickEvents.END_SERVER_TICK.register(server -> Messenger.resolveChunkCache());
-		ServerTickEvents.END_WORLD_TICK.register(Messenger::postAllPlayers);
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-			dispatcher.register(CommandManager.literal("blockCache").executes(context -> {
-				context.getSource().sendFeedback(() -> Text.literal("Blocks: " + Messenger.cacheSize()), false);
-				return 1;
-			}));
-			dispatcher.register(CommandManager.literal("chunkCache").executes(context -> {
-				context.getSource().sendFeedback(() -> Text.literal("Chunks: " + Messenger.chunkCacheSize()), false);
-				return 1;
-			}));
-		});
+    @Override
+    public void onInitialize () {
+        Settings.read();
+        ServerChunkEvents.CHUNK_LOAD.register(Messenger::cacheChunk);
+        ServerTickEvents.END_SERVER_TICK.register(server -> Messenger.resolveChunkCache());
+        ServerTickEvents.END_WORLD_TICK.register(Messenger::postAllPlayers);
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(CommandManager.literal("blockCache").executes(context -> {
+                context.getSource().sendFeedback(() -> Text.literal("Blocks: " + Messenger.cacheSize()), false);
+                return 1;
+            }));
+            dispatcher.register(CommandManager.literal("chunkCache").executes(context -> {
+                context.getSource().sendFeedback(() -> Text.literal("Chunks: " + Messenger.chunkCacheSize()), false);
+                return 1;
+            }));
+        });
 
-		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-			for (int i = 0; i < THREADS; ++i) {
-				callers[i] = new HttpCaller();
-				callThreads[i] = new Thread(null, callers[i], "Http thread " + i);
-				callThreads[i].start();
-			}
-		});
-		ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
-			for (HttpCaller caller : callers) {
-				caller.stop();
-			}
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            for (int i = 0; i < THREADS; ++i) {
+                callers[i] = new HttpCaller();
+                callThreads[i] = new Thread(null, callers[i], "Http thread " + i);
+                callThreads[i].start();
+            }
+        });
+        ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
+            for (HttpCaller caller : callers) {
+                caller.stop();
+            }
 
-			for (Thread thread : callThreads) {
-				try {
-					thread.join();
-				}
-				catch (InterruptedException ignored) {
+            for (Thread thread : callThreads) {
+                try {
+                    thread.join();
+                }
+                catch (InterruptedException ignored) {
 
-				}
-			}
-			Messenger.clear();
-		});
-	}
+                }
+            }
+            Messenger.clear();
+        });
+    }
 }
